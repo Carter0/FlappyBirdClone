c80b5a4f2b2a288c1ac199f89c6c8151198a0149
carter <weinbergcarter@gmail.com>
2021-07-26 20:32:16 -0400

    Finishing up the logic for the game

--- src/Main.elm
+++ src/Main.elm
@@ -47,6 +47,9 @@ type alias Memory =
     , pipeXPosition : Number
     , pipeHeights : ( PipeHeight, PipeHeight )
     , pipeGapModifier : Number
+    , isBirdDead : Bool
+    , score : Number
+    , pipeHasPassed: Bool
     }
 
 
@@ -62,6 +65,9 @@ initialMemory =
     , pipeXPosition = -3000
     , pipeHeights = ( 0, 0 )
     , pipeGapModifier = 0
+    , isBirdDead = False
+    , score = 0
+    , pipeHasPassed = False
     }
 
 
@@ -71,10 +77,24 @@ initialMemory =
 
 view : Computer -> Memory -> List Shape
 view computer memory =
-    [ drawBird birdXPosition memory.y
+    [ renderPlayerOrGameOver memory
     , drawBottomPipe computer.screen.bottom (Tuple.first memory.pipeHeights) memory.pipeXPosition
     , drawTopPipe computer.screen.top (Tuple.second memory.pipeHeights) memory.pipeXPosition
     ]
+
+
+renderPlayerOrGameOver : Memory -> Shape
+renderPlayerOrGameOver memory =
+    if memory.isBirdDead then
+        drawLoseText memory
+
+    else
+        drawBird birdXPosition memory.y
+
+
+drawLoseText : Memory -> Shape
+drawLoseText memory =
+    scale 3 (words black <| "You have died. Your score is: " ++ (String.fromFloat memory.score))
 
 
 drawBird : Number -> Number -> Shape
@@ -149,28 +169,76 @@ incrementPipeGap screen { pipeGapModifier, pipeXPosition } =
         pipeGapModifier
 
 
+isBirdOffScreen : Number -> Screen -> Bool
+isBirdOffScreen birdYPosition screen =
+    birdYPosition < screen.bottom
+
+
+isBirdTouchingPipe : Screen -> Memory -> Bool
+isBirdTouchingPipe screen { y, pipeXPosition, pipeHeights, pipeGapModifier } =
+    let
+        xPositionsColliding : Bool
+        xPositionsColliding =
+            pipeXPosition - pipeWidth < birdXPosition && pipeXPosition + pipeWidth > birdXPosition
+
+        topPipeColliding : PipeHeight -> Bool
+        topPipeColliding topPipeHeight =
+            y > screen.top - topPipeHeight / 2
+
+        bottomPipeColliding : PipeHeight -> Bool
+        bottomPipeColliding bottomPipeHeight =
+            y < screen.bottom + bottomPipeHeight / 2
+
+        yPositionsColliding : Bool
+        yPositionsColliding =
+            topPipeColliding (Tuple.second pipeHeights) || bottomPipeColliding (Tuple.first pipeHeights)
+    in
+    xPositionsColliding && yPositionsColliding
+
+
+checkGameOver : Screen -> Memory -> Bool
+checkGameOver screen memory =
+    if memory.isBirdDead then
+        True
+
+    else
+        isBirdOffScreen memory.y screen || isBirdTouchingPipe screen memory
+
+
+checkGameOver2 : Screen -> Memory -> Number
+checkGameOver2 screen memory =
+    if memory.isBirdDead then
+        screen.left - 1000
+    else
+        setPipeXPosition screen memory.pipeXPosition
+
+incrementScore : Memory -> Number
+incrementScore {score, pipeXPosition, pipeHasPassed, isBirdDead} =
+    if pipeXPosition < birdXPosition && not pipeHasPassed && (not isBirdDead) then
+        score + 1
+    else
+        score
+
+didPipeRespawn : Memory -> Bool
+didPipeRespawn memory =
+    memory.pipeXPosition > 0
+
+hasPipePassed : Memory -> Bool
+hasPipePassed memory =
+    if memory.pipeHasPassed then
+        not (didPipeRespawn memory)
+    else
+        memory.pipeXPosition < birdXPosition
+
 update : Computer -> Memory -> Memory
 update computer memory =
     { y = memory.y + memory.yVel
     , yVel = yVelocityChanges memory computer.keyboard
     , isUpPressed = computer.keyboard.up
-    , pipeXPosition = setPipeXPosition computer.screen memory.pipeXPosition
+    , pipeXPosition = checkGameOver2 computer.screen memory
     , pipeHeights = setPipeHeights computer.screen memory
     , pipeGapModifier = incrementPipeGap computer.screen memory
+    , isBirdDead = checkGameOver computer.screen memory
+    , score = incrementScore memory
+    , pipeHasPassed = hasPipePassed memory
     }
-
-
-
-{--
-1. Get a bird that reacts to gravity and moves
- - apply gravity to the bird
- - make the bird jump
-
-
-
-
-2. Pipes
-3. Collisions
-4. Death
-4. Score
---}
